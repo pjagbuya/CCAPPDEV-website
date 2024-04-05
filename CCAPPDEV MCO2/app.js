28b078f65b301b340955ef930aeaf2706664c7be
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2024-04-06 01:12:17 +0800

    Commiting final publishes

--- CCAPPDEV MCO2/app.js
+++ CCAPPDEV MCO2/app.js
@@ -11,9 +11,9 @@ const { Server } = require('socket.io');
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const { getDb, connectToDb } = require('./db')
+let db
 
-module.exports.mongoose= require('mongoose');
-module.exports.mongoose.connect('mongodb://localhost:27017/AnimoDB');
 
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
@@ -47,10 +47,18 @@ function successFn(res){
 
 // MongoDB setup
 // PRIMARY KEYS ARE AUTOMATIC
-const {MongoClient} = require('mongodb');
-const databaseURL = "mongodb://localhost:27017";
-const mongoClient = new MongoClient(databaseURL);
-
+// const {MongoClient} = require('mongodb');
+// const databaseURL = "mongodb://localhost:27017";
+// const mongoClient = new MongoClient(databaseURL);
+const port = process.env.PORT | 3000;
+connectToDb((err) => {
+  if(!err){
+    app.listen(port, () => {
+      console.log('app listening on port 3000')
+    })
+    db = getDb()
+  }
+})
 const databaseName = "AnimoDB";
 const collectionLogin = "User";
 
@@ -58,18 +66,18 @@ const collectionLogin = "User";
 
 
 
-mongoClient.connect().then(function(con){
-
-  console.log("Attempt to create!");
-  const dbo = mongoClient.db(databaseName);
-  db = dbo;
-
-  dbo.createCollection(collectionLogin)
-    .then(successFn)
-    .catch(function (err){
-      console.log('Collection already exists');
-  });
-}).catch(errorFn);
+// mongoClient.connect().then(function(con){
+//
+//   console.log("Attempt to create!");
+//   const dbo = mongoClient.db(databaseName);
+//   db = dbo;
+//
+//   dbo.createCollection(collectionLogin)
+//     .then(successFn)
+//     .catch(function (err){
+//       console.log('Collection already exists');
+//   });
+// }).catch(errorFn);
 
 
 
@@ -105,6 +113,35 @@ app.use("/", searchLabRouter);
 io.on('connection', (socket) => {
   console.log(`user connected ${socket.id}`);
 
+  socket.emit("reserveUpdate", currReservations);
+  socket.on('reserved', (dlsuID)=>{
+    currReservations = Reservation.find({userID:dlsuID});
+    socket.emit("reserveUpdate", currReservations);
+
+    socket.on('reserved', (dlsuID)=>{
+      currReservations = Reservation.find({userID:dlsuID});
+      socket.emit("reserveUpdate", currReservations);
+      socket.broadcast.emit("reserveUpdate", currReservations);
+    })
+
+
+    socket.on("join-room", function(roomID){
+      socket.join(roomID);
+    });
+
+    socket.on("leave-room", function(roomID){
+      socket.leave(roomID);
+    });
+
+    socket.on('disconnect', function(){
+      console.log(`user disconnected ${socket.id}`);
+    });
+
+    socket.broadcast.emit("reserveUpdate", currReservations);
+
+
+  });
+
   socket.on("send-message", function(data){
     io.emit("recieve-message", data);
   });
@@ -112,10 +149,4 @@ io.on('connection', (socket) => {
   socket.on('disconnect', function(){
     console.log(`user disconnected ${socket.id}`);
   });
-});
-
-
-const port = process.env.PORT | 9090;
-server.listen(port, function(){
-    console.log('Listening at port '+port);
 });
